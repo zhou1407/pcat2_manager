ff5484f4edb7a4c6f2c70b4a67fcf8edb3158245
Kyosuke Nekoyashiki <supercatexpert@gmail.com>
2025-03-10 12:30:18 +0900

    Added test script.

---
+++ test/test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import socket
+import os
+
+s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+s.connect('/tmp/pcat-manager.sock')
+
+s.send(b"{'command':'pmu-fw-version-get'}\0")
+s.recv(256)
